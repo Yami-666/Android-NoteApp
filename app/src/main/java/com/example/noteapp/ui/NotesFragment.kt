97a0c575d33b98aee4abe5bff1122c708bd00d41
Yami-666 <trishin.m.2002@gmail.com>
2021-01-28 13:08:30 +0300

    Made add screen and simple navigation

--- app/src/main/java/com/example/noteapp/ui/NotesFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/NotesFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment() {
+
+    private var _binding: FragmentNotesBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_notes, container, false)
+        _binding = FragmentNotesBinding.bind(view)
+
+        binding.addNoteButton.setOnClickListener {
+            findNavController().navigate(R.id.action_notesFragment_to_addNoteFragment)
+        }
+        return view
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/noteapp/ui/add/AddNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/add/AddNoteFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.ComponentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentAddNoteBinding
 
 class AddNoteFragment : Fragment() {
+
+    private var _binding: FragmentAddNoteBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_add_note, container, false)
+        _binding = FragmentAddNoteBinding.bind(view)
+
+        binding.imageArrowBack.setOnClickListener {
+            findNavController().navigate(R.id.action_addNoteFragment_to_notesFragment)
+        }
+
+        return view
     }
 }
